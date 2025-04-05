ad6cf0099b49d19f1c3619227db1c87e0b8dcd98
Bagas <bagasaldianata20@gmail.com>
2025-04-05 18:03:46 +0700

    Membuat TopAppBar

--- app/src/main/java/com/bagas0060/scorecalc/MainActivity.kt
+++ app/src/main/java/com/bagas0060/scorecalc/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.bagas0060.scorecalc
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.bagas0060.scorecalc.ui.theme.ScoreCalcTheme
 
@@ -19,29 +25,48 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScoreCalcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen(){
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.app_name),
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.largeTopAppBarColors(
+                    containerColor = colorResource(id = R.color.red),
+                    titleContentColor = colorResource(id = R.color.white)
+                )
+
+            )
+        }
+    ){
+        innerPadding -> ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier){
     Text(
-        text = "Hello $name!",
+        text = "Hello Android!",
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     ScoreCalcTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
