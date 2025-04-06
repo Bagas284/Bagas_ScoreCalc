09c18fee1423d30d36c01cab259b2333d2b38c04
Bagas <bagasaldianata20@gmail.com>
2025-04-06 12:24:31 +0700

    Membuat Tampilan Home Screen

--- app/src/main/java/com/bagas0060/scorecalc/MainActivity.kt
+++ app/src/main/java/com/bagas0060/scorecalc/MainActivity.kt
@@ -5,18 +5,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bagas0060.scorecalc.ui.theme.ScoreCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,17 +67,68 @@ fun MainScreen(){
 
             )
         }
-    ){
-        innerPadding -> ScreenContent(Modifier.padding(innerPadding))
+    ){ innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
     }
 }
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier){
-    Text(
-        text = "Hello Android!",
+    Column (
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.kalkulator),
+            contentDescription = stringResource(R.string.deskripsi_home),
+            modifier = Modifier.padding(top = 30.dp)
+        )
+        Text(
+            text = stringResource(R.string.judul),
+            fontSize = MaterialTheme.typography.displaySmall.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.isi),
+            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+            lineHeight = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 5.dp)
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 15.dp)
+        ) {
+            ElevatedButton (
+                onClick = {},
+                modifier = Modifier
+                    .height(50.dp)
+                    .weight(1f)
+                    .padding(end = 4.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(colorResource(R.color.red))
+            ) {
+                Text(text = stringResource(R.string.b_nilaiPerMatkul),
+                    color = Color.White)
+            }
+
+            ElevatedButton (
+                onClick = {},
+                modifier = Modifier
+                    .height(50.dp)
+                    .weight(1f)
+                    .padding(start = 4.dp),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(colorResource(R.color.red))
+            ) {
+                Text(text = stringResource(R.string.b_nilaiSeluruh), color = Color.White)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
