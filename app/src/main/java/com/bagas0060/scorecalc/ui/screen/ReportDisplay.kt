ea5ed3eb6d72738d22aaecb5cb85acfaf05b87fe
Bagas <bagasaldianata20@gmail.com>
2025-06-09 19:19:14 +0700

    Membuat indikator loading image

--- app/src/main/java/com/bagas0060/scorecalc/ui/screen/ReportDisplay.kt
+++ app/src/main/java/com/bagas0060/scorecalc/ui/screen/ReportDisplay.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -98,6 +99,7 @@ fun ListItem(ipsemesterimage: IpSemesterImage){
                 .build(),
             contentDescription = stringResource(R.string.gambar, ipsemesterimage.gambar),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(R.drawable.loading_img),
             modifier = Modifier.fillMaxSize()
         )
 
